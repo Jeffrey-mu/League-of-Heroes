44d7b6723268c5aa25aa5737dd9de8cf61b4af54
Jeffrey <1115378579@qq.com>
2022-07-28 23:23:57 +0800

    chore： 删除注释

--- src/store/state.ts
+++ src/store/state.ts
@@ -3,9 +3,6 @@ import type { GOODS_ITME } from '~/composables/goods'
 import type { HEOR_ITME } from '~/composables/Heroes'
 import type { toArray } from '~/types'
 export const useStore = defineStore('state', () => {
-  /**
-   * Current name of the user.
-   */
   const goodsList = ref<toArray<GOODS_ITME>>([])
   const horeList = ref<toArray<HEOR_ITME>>([])
   function setGoodsList(data: toArray<GOODS_ITME>) {
@@ -14,12 +11,6 @@ export const useStore = defineStore('state', () => {
   function setHoreList(data: toArray<HEOR_ITME>) {
     horeList.value = data
   }
-  /**
-   * Changes the current name of the user and saves the one that was used
-   * before.
-   *
-   * @param name - new name to set
-   */
 
   return {
     goodsList,
